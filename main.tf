a15bd56d7d3a55342991500b1d31a735c2a5d2f2
Nithinsettibathula <nithinsettibathula1426@gmail.com>
2026-02-25 15:48:23 +0530

    Fix: Fully mapped all module arguments in root main.tf

--- main.tf
+++ main.tf
@@ -1,68 +1,53 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# --- Networking Module ---
+module "networking" {
+  source          = "./modules/networking"
+  resource_prefix = var.resource_prefix
+  vpc_id          = var.vpc_id
+  public_subnets  = var.public_subnets
+  # Passing the required private subnets argument
+  private_subnets = var.private_subnets 
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-# 1. ECR Module
+# --- ECR Module ---
 module "ecr" {
   source          = "./modules/ecr"
   resource_prefix = var.resource_prefix
 }
 
-# 2. Networking Module
-module "networking" {
-  source          = "./modules/networking"
-  vpc_id          = var.vpc_id
-  public_subnets  = var.public_subnets
-  resource_prefix = var.resource_prefix
-}
-
-# 3. ECS Module
-# Note: We move ECS above RDS so we can get its Security Group ID
-module "ecs" {
-  source              = "./modules/ecs"
-  vpc_id              = var.vpc_id
-  private_subnets     = var.private_subnets
-  resource_prefix     = var.resource_prefix
-  execution_role_arn  = var.execution_role_arn
-  ecr_repository_url  = module.ecr.repository_url
-  alb_sg_id           = module.networking.alb_sg_id
-  blue_tg_arn         = module.networking.blue_tg_arn
-  
-  # Temporary database values until RDS is created
-  db_host             = module.rds.db_host
-  db_username         = "strapi"
-  db_password         = var.db_password
-}
-
-# 4. RDS Module
+# --- RDS Module ---
 module "rds" {
   source          = "./modules/rds"
+  resource_prefix = var.resource_prefix
   vpc_id          = var.vpc_id
   private_subnets = var.private_subnets
-  resource_prefix = var.resource_prefix
   db_password     = var.db_password
-  
-  # Passing the ECS SG ID from the ECS module output
-  ecs_sg_id       = module.ecs.ecs_sg_id
 }
 
-# 5. CodeDeploy Module
+# --- ECS Module ---
+module "ecs" {
+  source             = "./modules/ecs"
+  resource_prefix    = var.resource_prefix
+  vpc_id             = var.vpc_id
+  # Ensuring ECS has access to both subnet types for routing
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  execution_role_arn = var.execution_role_arn
+  alb_sg_id          = module.networking.alb_sg_id
+  blue_tg_arn        = module.networking.blue_tg_arn
+  db_host            = module.rds.db_instance_endpoint
+  db_username        = "postgres"
+  db_password        = var.db_password
+  ecr_repository_url = module.ecr.repository_url
+}
+
+# --- CodeDeploy Module ---
 module "codedeploy" {
-  source                  = "./modules/codedeploy"
-  resource_prefix         = var.resource_prefix
-  codedeploy_role_arn     = var.codedeploy_role_arn
-  ecs_cluster_name        = module.ecs.cluster_name
-  ecs_service_name        = module.ecs.service_name
-  alb_listener_arn        = module.networking.listener_arn
-  blue_target_group_name  = module.networking.blue_tg_name
-  green_target_group_name = module.networking.green_tg_name
+  source              = "./modules/codedeploy"
+  resource_prefix     = var.resource_prefix
+  codedeploy_role_arn = var.codedeploy_role_arn
+  ecs_cluster_name    = module.ecs.cluster_name
+  ecs_service_name    = module.ecs.service_name
+  alb_listener_arn    = module.networking.listener_arn
+  blue_tg_name        = module.networking.blue_tg_name
+  green_tg_name       = module.networking.green_tg_name
 }
