f4cc51087474550a6aa22dbecfae6ce67739b546
Nithinsettibathula <nithinsettibathula1426@gmail.com>
2026-02-25 15:49:55 +0530

    Fix: Added public_subnets variable to ECS module

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,10 +1,15 @@
 variable "resource_prefix" {}
 variable "vpc_id" {}
+variable "private_subnets" { type = list(string) }
+variable "execution_role_arn" {}
+variable "alb_sg_id" {}
+variable "blue_tg_arn" {}
+variable "db_host" {}
+variable "db_username" {}
+variable "db_password" {}
+variable "ecr_repository_url" {}
 
+# Add this missing block:
 variable "public_subnets" {
-  type = list(string)
-}
-
-variable "private_subnets" {
   type = list(string)
 }
