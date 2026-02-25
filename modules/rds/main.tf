91ef20c9aa641a9b5d77b53de96f8b57221b025a
Nithinsettibathula <nithinsettibathula1426@gmail.com>
2026-02-25 16:11:33 +0530

    Final Infrastructure: RDS and CodeDeploy modules complete

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -8,7 +8,8 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [var.ecs_sg_id] # Restrict access to ECS tasks only
+    # This uses the variable we passed from the ECS output
+    security_groups = [var.ecs_sg_id] 
   }
 
   egress {
@@ -30,7 +31,7 @@ resource "aws_db_instance" "strapi_db" {
   identifier           = "${var.resource_prefix}-db"
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "16" # Updated to a stable version
+  engine_version       = "16"
   instance_class       = "db.t3.micro"
   db_name              = "strapi"
   username             = "postgres"
@@ -39,8 +40,4 @@ resource "aws_db_instance" "strapi_db" {
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-}
-
-output "db_instance_endpoint" {
-  value = aws_db_instance.strapi_db.endpoint
 }
